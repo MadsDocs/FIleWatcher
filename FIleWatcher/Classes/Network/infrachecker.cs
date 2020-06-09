e84264cc7144edcff5516999fe746da574a494e0
MadsDocs <waterfoxbrowser@gmail.com>
2020-06-09 23:39:50 +0200

    Addition to https://hoanger.atlassian.net/browse/FILEWATCH-6, this method should check if the server is up.

---
+++ FIleWatcher/Classes/Network/infrachecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Windows.Forms;
+
+namespace FileWatcher.Classes.Network
+{
+    class infrachecker
+    {
+        public void checkInfra()
+        {
+            //Diese Methode soll checken ob alle Server verfügbar sind
+            //Derzeit ist es genau ein Server (FTP) aber vl. werden es ja mal mehr...
+
+
+            try
+            {
+                FtpWebRequest testrequest = (FtpWebRequest)WebRequest.Create("84.115.70.116");
+                testrequest.Credentials = new NetworkCredential("", "");
+                testrequest.Method = WebRequestMethods.Ftp.ListDirectory;
+
+                FtpWebResponse response = (FtpWebResponse)testrequest.GetResponse();
+
+                if ( response.StatusCode == FtpStatusCode.NeedLoginAccount)
+                {
+                    MessageBox.Show("FTP Server is up");
+                }
+                else if ( response.StatusCode == FtpStatusCode.CantOpenData)
+                {
+                    MessageBox.Show("FTP Server is not up");
+                }
+                    
+
+            }
+            catch ( Exception ex)
+            {
+
+            }
+        }
+    }
+}
